2b00cebaa61a1206d247a9adf77b55236d48748d
Anoop Raju <anoopraju31@gmail.com>
2024-01-01 13:03:15 +0530

    Module 4 - JSX added

--- module 4/react-bundling/src/index.js
+++ module 4/react-bundling/src/index.js
@@ -1,20 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const child = React.createElement(
-	'p',
-	{},
-	'Hello from Anoop, I am inside react now.',
+const div = (
+	<div>
+		<p className='text'> Happy New Year </p>
+	</div>
 )
-const child1 = React.createElement('p', {}, 'This a child.')
-const child2 = React.createElement('p', {}, 'This is another child.')
-const div = React.createElement('div', { className: 'text' }, [
-	child,
-	child1,
-	child2,
-])
-
-console.log(div)
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(div)
