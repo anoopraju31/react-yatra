9c783fc9b8ce01a235c19b32f2359e84a8b3d739
anoopraju31 <anoopraju31@gmail.com>
2024-01-01 14:24:54 +0530

    Module 5 - React - Button Component added

--- module 5/react-bundling/src/App.jsx
+++ module 5/react-bundling/src/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Button from './components/Button.jsx'
 
 const App = () => {
-	return <div>App</div>
+	return (
+		<div>
+			<Button />
+		</div>
+	)
 }
 
 export default App
