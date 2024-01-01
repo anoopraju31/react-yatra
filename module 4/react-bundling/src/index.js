d7d714f9e8a9eaadbca41e8c4db6558cf817a908
anoopraju31 <anoopraju31@gmail.com>
2024-01-01 13:08:59 +0530

    Module 4 - Conditional rendering in JSX

--- module 4/react-bundling/src/index.js
+++ module 4/react-bundling/src/index.js
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const API_DATA = 'Hello World, Happy New Year'
+const isMorning = new Date().getHours() < 12
 
 const div = (
 	<div>
 		<p className='text'> {API_DATA} </p>
+		{isMorning && <p> Good Morning! </p>}
 	</div>
 )
 
