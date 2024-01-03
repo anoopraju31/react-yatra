1d3b75071f4dcd6ea5ae65f64a7591b864403535
anoopraju31 <anoopraju31@gmail.com>
2024-01-03 20:57:07 +0530

    Module 9 - React - useEffect added

--- module 9/react/src/App.jsx
+++ module 9/react/src/App.jsx
@@ -1,17 +1,29 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Button from './components/Button.jsx'
 
 const App = () => {
-	const [message, setMessage] = useState('Hello User, Good Morning!')
-	const changeMessage = () => {
-		console.log('Clicked')
-		setMessage('Hello User, Good Afternoon')
+	const [data, setData] = useState(0)
+
+	useEffect(() => {
+		console.log('I am run everytime there is a re-render')
+	})
+
+	useEffect(() => {
+		console.log('I am updating')
+	}, [])
+
+	useEffect(() => {
+		console.log('I will run everytime data gets updated.')
+	}, [data])
+
+	const handleClick = () => {
+		setData((prev) => prev + 1)
 	}
 
 	return (
 		<div>
-			<p>{message}</p>
-			<Button handleClick={changeMessage}> Click Me </Button>
+			<p>{data}</p>
+			<Button handleClick={handleClick}> Update </Button>
 		</div>
 	)
 }
