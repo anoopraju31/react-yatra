0f92e564d6235a2ec061aed840be4f15807aac56
anoopraju31 <anoopraju31@gmail.com>
2024-01-04 13:33:39 +0530

    module 10 React - App Component clean up

--- module 10/react/src/App.jsx
+++ module 10/react/src/App.jsx
@@ -1,31 +1,7 @@
-import React, { useEffect, useState } from 'react'
-import Button from './components/Button.jsx'
+import React from 'react'
 
 const App = () => {
-	const [data, setData] = useState(0)
-
-	useEffect(() => {
-		console.log('I am run everytime there is a re-render')
-	})
-
-	useEffect(() => {
-		console.log('I am updating')
-	}, [])
-
-	useEffect(() => {
-		console.log('I will run everytime data gets updated.')
-	}, [data])
-
-	const handleClick = () => {
-		setData((prev) => prev + 1)
-	}
-
-	return (
-		<div>
-			<p>{data}</p>
-			<Button handleClick={handleClick}> Update </Button>
-		</div>
-	)
+	return <div></div>
 }
 
 export default App
