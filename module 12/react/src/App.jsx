3d8907da75adeb2cf26ecc239d466bc34852f9a2
anoopraju31 <anoopraju31@gmail.com>
2024-01-05 18:44:25 +0530

    Module 12 - React App - useCallback added

--- module 12/react/src/App.jsx
+++ module 12/react/src/App.jsx
@@ -1,70 +1,22 @@
-import React, { useEffect, useRef, useState } from 'react'
-import Button from './components/Button.jsx'
-import Input from './components/Input.jsx'
+import React, { useState } from 'react'
+import SecondParent from './components/SecondParent.jsx'
 
 const App = () => {
-	const [timer, setTimer] = useState(0)
 	const [count, setCount] = useState(0)
-	const countRef = useRef(0)
-	const timerRef = useRef(null)
-	const headingRef = useRef(null)
-	const inputRef = useRef(null)
-	const paragraphRef = useRef(null)
-	let countLocal = 0
-
-	const handleCountRef = () => {
-		countRef.current += 1
-	}
-	const handleCountState = () => setCount((previousCount) => previousCount + 1)
-	const handleLocalCount = () => countLocal++
-
-	useEffect(() => {
-		const timerId = setInterval(() => setTimer((prev) => prev + 1), 1000)
-		timerRef.current = timerId
-
-		return () => {
-			clearInterval(timerId)
-		}
-	}, [timerRef])
-
-	useEffect(() => {
-		console.log('ref change', timerRef)
-	}, [timerRef])
-
-	useEffect(() => {
-		if (!inputRef.current) return
-
-		paragraphRef.current.innerText = JSON.stringify(
-			inputRef.current.getBoundingClientRect(),
-		)
-	}, [inputRef])
-
-	const handleClearTimer = () => {
-		console.log(timerRef.current)
-		clearInterval(timerRef.current)
-		timerRef.current = null
-		setTimer(0)
-	}
-
-	const changeHeading = () => {
-		headingRef.current.innerText = 'changed Heading'
+	const myFunction = () => {
+		//
 	}
 
 	return (
-		<div>
-			<Button handleClick={handleClearTimer}> clear Timer </Button>
-			<Button handleClick={changeHeading}> change Heading </Button>
-			<Button handleClick={handleLocalCount}> Increment Local Count </Button>
-			<Button handleClick={handleCountRef}> Increment Ref Count </Button>
-			<Button handleClick={handleCountState}> Increment State Count </Button>
-
-			<p> Local Count: {countLocal} </p>
-			<p> ref count: {countRef.current} </p>
-			<p> state count: {count} </p>
-			<p> Timer: {timer}</p>
-			<h1 ref={headingRef}> Heading </h1>
-			<p ref={paragraphRef}></p>
-			<Input ref={inputRef} />
+		<div
+			style={{
+				background: '#0f3',
+				padding: '20px',
+			}}>
+			<h1> The outermost Parent </h1>
+			<p>{count}</p>
+			<button onClick={() => setCount((prev) => prev + 1)}> Increment </button>
+			<SecondParent />
 		</div>
 	)
 }
