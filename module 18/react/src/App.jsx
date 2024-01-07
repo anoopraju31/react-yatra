a698f9f652d4b7291075b1b5fd0981756468011e
anoopraju31 <anoopraju31@gmail.com>
2024-01-07 12:12:14 +0530

    Module 18 - React App - redux added

--- module 18/react/src/App.jsx
+++ module 18/react/src/App.jsx
@@ -1,31 +1,31 @@
 import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import {
+	decrement,
+	decrementByAmount,
+	increment,
+	incrementByAmount,
+} from './feature/CounterSlice'
 
 const App = () => {
-	const [showModel, setShowModel] = useState(false)
-	return (
-		<div
-			style={{
-				overflow: showModel ? 'hidden' : 'auto',
-			}}>
-			<button onClick={() => setShowModel(true)}> Open Model </button>
+	const [amount, setAmount] = useState(1)
+	const counter = useSelector((state) => state.counter.value)
+	const dispatch = useDispatch()
 
-			{showModel && (
-				<div
-					style={{
-						margin: 0,
-						position: 'fixed',
-						top: '0px',
-						left: '0px',
-						width: '100vw',
-						height: '100vh',
-						display: 'flex',
-						justifyContent: 'center',
-						alignItems: 'center',
-						background: '#0f4',
-					}}>
-					<button onClick={() => setShowModel(false)}> Close Model </button>
-				</div>
-			)}
+	const handleAmountChange = (e) => setAmount(parseInt(e.target.value))
+	const handleIncrement = () => dispatch(increment())
+	const handleDecrement = () => dispatch(decrement())
+	const handleIncrementByAmount = () => dispatch(incrementByAmount(amount))
+	const handleDecrementByAmount = () => dispatch(decrementByAmount(amount))
+
+	return (
+		<div>
+			<p> {counter} </p>
+			<input type='number' value={amount} onChange={handleAmountChange} />
+			<button onClick={handleIncrement}> Increment </button>
+			<button onClick={handleDecrement}> Decrement </button>
+			<button onClick={handleIncrementByAmount}> Increment By Amount </button>
+			<button onClick={handleDecrementByAmount}> Decrement By Amount </button>
 		</div>
 	)
 }
