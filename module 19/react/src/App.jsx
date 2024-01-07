ce5a510e6e5348a5f39a4e292c64a86374b0d301
anoopraju31 <anoopraju31@gmail.com>
2024-01-07 13:00:50 +0530

    Module 19 - React App - Router added

--- module 19/react/src/App.jsx
+++ module 19/react/src/App.jsx
@@ -1,32 +1,13 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
-	const [showModel, setShowModel] = useState(false)
 	return (
-		<div
-			style={{
-				overflow: showModel ? 'hidden' : 'auto',
-			}}>
-			<button onClick={() => setShowModel(true)}> Open Model </button>
-
-			{showModel && (
-				<div
-					style={{
-						margin: 0,
-						position: 'fixed',
-						top: '0px',
-						left: '0px',
-						width: '100vw',
-						height: '100vh',
-						display: 'flex',
-						justifyContent: 'center',
-						alignItems: 'center',
-						background: '#0f4',
-					}}>
-					<button onClick={() => setShowModel(false)}> Close Model </button>
-				</div>
-			)}
-		</div>
+		<Routes>
+			<Route path='/' element={<h1> Home Page </h1>} />
+			<Route path='/about' element={<h1> About Page </h1>} />
+			<Route path='/contact' element={<h1> Contact Page </h1>} />
+		</Routes>
 	)
 }
 
